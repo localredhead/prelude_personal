cd9e7dc4457a5a09cf85fed851befbdd522a24b5
localredhead <levi.strope@gmail.com>
2017-01-16 17:43:58 -0800

    blog settings

--- meego.el
+++ meego.el
@@ -1,26 +1,26 @@
 (require 'ego)
 (ego-add-to-alist 'ego-project-config-alist
-                  `(("localredhead"
+                  '(("localredhead"
                      :repository-directory "~/Projects/localredhead.github.io"
                      :repository-org-branch "source" ;
                      :repository-html-branch "master" ;
                      :site-domain "https://localredhead.github.io"
-                     :site-main-title "Levi Strope"
+                     :site-main-title "L. Strope"
                      :site-sub-title "entropy in action"
                      :theme (dracula)
-                     :summary (("year" :posts) ("tags" :tags))
+                     :summary (("years" :year :updates 50) ("tags" :tags))
+                     :default-category "blog"
+                     :about ("About me" "/about/")
                      :source-browse-url ("Github" "https://github.com/localredhead")
-                     :personal-avatar "https://media.licdn.com/media/p/6/005/095/364/3fd9ba3.jpg"
+                     :personal-avatar "https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/12801483_10154713451449863_4966949962466084989_n.jpg?oh=302271f4d9a4f537c27c9f5b2bb64c74&oe=58DE6D0C"
+                     ;; :personal-avatar "https://media.licdn.com/media/p/6/005/095/364/3fd9ba3.jpg"
                      :confound-email t
                      :ignore-file-name-regexp "readme.org"
-                     :category-ignore-list ("themes" "assets" "notes")
-                     )
-
+                     :category-ignore-list ("themes" "assets" "notes" "moleskin"))
                     ;; more projects or sites config can be added here
-
                     ))
 
-;;:summary (("years" :year :updates 10) ("authors" :authors) ("tags" :tags))
+;; :summary (("years" :year :updates 10) ("authors" :authors) ("tags" :tags))
 ;; :repository-directory nil
 ;; :site-domain nil
 ;; :site-main-title "ego"
